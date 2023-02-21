d67657fbe7b708e241086813b197e6ff13f18ed3
Eric Johnson <eric.johnson@aware3.com>
2023-02-21 08:38:59 -0500

    Lesson10

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,9 @@ Route::get('/post/{post}', function ($slug) {
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", 5, function () use ($path) {
+        return file_get_contents($path);
+    });
 
     return view ('post', [
         'post' => $post
