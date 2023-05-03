6e1397d93abf8e9effea9f6be1c56ffa8887fff4
Eric Johnson <roerjo.personal@gmail.com>
2023-05-03 08:38:40 -0400

    Lesson 14 & 15

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+
+<link href="/app.css" rel="stylesheet">
+
+<body>
+    {{ $slot }}
+</body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-    <title>My Blog</title>
-
-    <link href="/app.css" rel="stylesheet">
-
-    <body>
-        @foreach ($posts as $post)
-            <article>
-                <h1><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></h1>
-                <div>
-                    {{ $post->excerpt }}
-                </div>
-            </article>
-        @endforeach
-    </body>
+<x-layout>
+    @foreach ($posts as $post)
+        <article>
+            <h1><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+</x-layout>
