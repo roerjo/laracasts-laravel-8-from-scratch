c1a2f0214a2e658d31e17b5b4f4d476a490ea8f3
Eric Johnson <roerjo.personal@gmail.com>
2023-05-08 09:37:59 -0400

    Lesson 24

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,6 +2,11 @@
     @foreach ($posts as $post)
         <article>
             <h1><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></h1>
+
+            <p>
+                <a href="#">{{ $post->category->name }}</a>
+            </p>
+
             <div>
                 {{ $post->excerpt }}
             </div>
