c62bd52e1aef10db17b234331a7d678db14c24f3
Eric Johnson <roerjo.personal@gmail.com>
2023-05-09 08:38:28 -0400

    Lesson 27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +18,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family',
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My First Personal',
+            'slug' => 'my-first-personal',
+            'excerpt' => 'Lorum ipsum sit met.',
+            'body' => 'Lorum ipsum sit met. Lorum ipsum sit met.Lorum ipsum sit met.',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My First Work',
+            'slug' => 'my-first-work',
+            'excerpt' => 'Lorum ipsum sit met.',
+            'body' => 'Lorum ipsum sit met. Lorum ipsum sit met.Lorum ipsum sit met.',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My First Family',
+            'slug' => 'my-first-post',
+            'excerpt' => 'Lorum ipsum sit met.',
+            'body' => 'Lorum ipsum sit met. Lorum ipsum sit met.Lorum ipsum sit met.',
+        ]);
     }
 }
