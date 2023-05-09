cdabd01e8ef6be82afbf4e6d83a6852b37ef3d83
Eric Johnson <roerjo.personal@gmail.com>
2023-05-09 08:50:45 -0400

    Lesson 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,52 +18,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
-        ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My First Personal',
-            'slug' => 'my-first-personal',
-            'excerpt' => 'Lorum ipsum sit met.',
-            'body' => 'Lorum ipsum sit met. Lorum ipsum sit met.Lorum ipsum sit met.',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My First Work',
-            'slug' => 'my-first-work',
-            'excerpt' => 'Lorum ipsum sit met.',
-            'body' => 'Lorum ipsum sit met. Lorum ipsum sit met.Lorum ipsum sit met.',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My First Family',
-            'slug' => 'my-first-post',
-            'excerpt' => 'Lorum ipsum sit met.',
-            'body' => 'Lorum ipsum sit met. Lorum ipsum sit met.Lorum ipsum sit met.',
-        ]);
+        Post::factory(5)->create();
     }
 }
