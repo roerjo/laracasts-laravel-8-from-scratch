82b4e5c183d1a38847e057f4c900267e56ccedf8
Eric Johnson <roerjo.personal@gmail.com>
2023-05-11 10:43:27 -0400

    Lesson 34

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -13,27 +13,46 @@
 
     <div class="space-y-3 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-101 rounded-xl">
-            <select class="flex-2 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>Category
-                </option>
-                <option value="personal">Personal</option>
-                <option value="business">Business</option>
-            </select>
+        <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
 
-            <svg class="transform -rotate-91 absolute pointer-events-none" style="right: 12px;" width="22"
-                    height="21" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#037777777777" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#221"
-                            d="M12.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                </g>
-            </svg>
+            <div x-data="{ show: false }" @click.away="show = false">
+                <button
+                    @click="show = !show"
+                    class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
+                    >
+                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
+
+                    <svg class="transform -rotate-91 absolute pointer-events-none" style="right: 12px;" width="22"
+                            height="21" viewBox="0 0 22 22">
+                        <g fill="none" fill-rule="evenodd">
+                            <path stroke="#037777777777" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                            </path>
+                            <path fill="#221"
+                                    d="M12.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                        </g>
+                    </svg>
+                </button>
+
+                    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" style="display: none">
+                        <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
+                            All
+                        </a>
+                        @foreach($categories as $category)
+                            <a href="/categories/{{ $category->slug }}"
+                                class="
+                                    block text-left px-3 text-sm leading-6
+                                    hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white
+                                    {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}
+                                ">
+                                {{ ucwords($category->name) }}
+                            </a>
+                        @endforeach
+                    </div>
+            </div>
         </div>
 
         <!-- Other Filters -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-101 rounded-xl">
+        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
             <select class="flex-2 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
                 <option value="category" disabled selected>Other Filters
                 </option>
@@ -55,7 +74,7 @@
         </div>
 
         <!-- Search -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-101 rounded-xl px-3 py-2">
+        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
                 <input type="text" name="search" placeholder="Find something"
                         class="bg-transparent placeholder-black font-semibold text-sm">
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts', [
         'posts' => Post::latest()->with('category', 'author')->get(),
+        'categories' => Category::all(),
     ]);
 });
 
@@ -31,11 +32,14 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts->load('category', 'author'),
+        'currentCategory' => $category,
+        'categories' => Category::all(),
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts->load('category'),
+        'categories' => Category::all(),
     ]);
 });
