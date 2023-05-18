c5780367b41affd10133d7db7be87b7143c6a75e
Eric Johnson <roerjo.personal@gmail.com>
2023-05-18 08:35:03 -0400

    Lesson 39 40 41

---
+++ resources/views/components/category-dropdown.blade.php
@@ -0,0 +1,21 @@
+<x-dropdown>
+    <x-slot name="trigger">
+        <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
+            {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
+
+            <x-icon name='dropdown-arrow' class="absolute pointer-events-none"  style="right: 12px;"/>
+        </button>
+    </x-slot>
+
+    <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+
+    @foreach($categories as $category)
+                <!-- {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }} -->
+        <x-dropdown-item
+            href="/?category={{ $category->slug }}"
+            :active="isset($currentCategory) && $currentCategory->is($category)"
+            >
+            {{ ucwords($category->name) }}
+        </x-dropdown-item>
+    @endforeach
+</x-dropdown>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts->load('category', 'author'),
-        'currentCategory' => $category,
-        'categories' => Category::all(),
-    ]);
-})->name('category');
-
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts->load('category'),
-        'categories' => Category::all(),
-    ]);
-});
