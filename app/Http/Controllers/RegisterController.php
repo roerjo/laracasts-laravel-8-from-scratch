e4e238c9d2b28ddf03fdcf58b48836f6ca8e2aed
Eric Johnson <roerjo.personal@gmail.com>
2023-05-23 08:37:23 -0400

    Lesson 46

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -20,6 +20,7 @@ class RegisterController extends Controller
             'password' => 'required|min:7|max:255',
         ]);
 
+
         User::create($attributes);
 
         return redirect('/');
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
