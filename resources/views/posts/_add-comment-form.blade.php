1168c9adbdac49bbecc25567d19c7d75b5ea9bd4
Eric Johnson <roerjo.personal@gmail.com>
2023-05-26 09:00:26 -0400

    Lesson 57

---
+++ resources/views/posts/_add-comment-form.blade.php
@@ -0,0 +1,35 @@
+@auth
+    <x-panel>
+        <form method="POST" action="/posts/{{ $post->slug }}/comments">
+            @csrf
+
+            <header class="flex items-center">
+                <img src="https://i.pravatar.cc/60?u={{ auth()->user()->id }}" alt="" width="40" height="40" class="rounded-all">
+
+                <h2 class="ml-4">Want to particpate?</h2>
+            </header>
+
+            <div class="mt-6">
+                <textarea
+                    name="body"
+                    class="w-full text-sm focus:outline-none focus:ring"
+                    rows="5"
+                    placeholder="Quick, think of something to say!"
+                    required
+                    ></textarea>
+
+                @error('body')
+                    <span class="text-sm text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="flex justify-end mt-8 pt-6 border-t border-gray-200">
+                <x-submit-button>Post</x-submit-button>
+            </div>
+        </form>
+    </x-panel>
+@else
+    <p class="font-semibold">
+        <a href="/register" class="text-blue-500 hover:underline">Register</a> or <a href="/login" class="text-blue-500 hover:underline">log in</a> to particate in the conversation.
+    </p>
+@endauth
