c93240c5bd3343983e4a3eeca7a63de37d5d89bb
Eric Johnson <roerjo.personal@gmail.com>
2023-06-05 09:03:28 -0400

    Lesson 59

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
+use MailchimpMarketing\ApiClient;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
@@ -17,3 +18,27 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+Route::post('newsletter', function () {
+    request()->validate(['email' => 'required|email']);
+
+    $mailchimp = new ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us11',
+    ]);
+
+    try {
+        $response = $mailchimp->lists->addListMember('e05afb559c', [
+            'email_address' => request('email'),
+            'status' => 'subscribed',
+        ]);
+    } catch (\Throwable $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
+
+    return redirect('/')->with('success', 'You are now subscribed!');
+});
