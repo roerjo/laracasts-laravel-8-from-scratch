8194151ed5549c2cd74c065254d43232018685b3
Eric Johnson <roerjo.personal@gmail.com>
2023-06-07 08:52:16 -0400

    Lesson 65

---
+++ resources/views/components/form/button.blade.php
@@ -0,0 +1,8 @@
+<x-form.field>
+    <button
+        type="submit"
+        class="bg-blue-500 text-white uppercase font-semibold text-sm py-2 px-10 rounded-2xl hover:bg-blue-500"
+        >
+        {{ $slot }}
+    </button>
+</x-form.field>
---
+++ resources/views/components/form/label.blade.php
@@ -0,0 +1,7 @@
+@props(['name', 'for'])
+
+<label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+        for="{{ $for ?? $name }}"
+>
+    {{ ucwords($name) }}
+</label>
